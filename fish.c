e535b3a4afe1d49811fb03f5393ad3cf435848ff
webnai <wilbertboadzo144@gmail.com>
2023-04-09 10:46:17 +0000

    :bug: all bugs fixed
    debuged files

--- fish.c
+++ fish.c
@@ -1,22 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-    float n,i,j,t,s;
+    float n, i, j, t, s;
     printf("Enter the size of fish: ");
     scanf("%f", &n);
-    n=n+14;
-    s=n+14;
-    s=n/10;
-    t=4.72/n;
-    for(j=floor(s);j>=-ceil(s);j--)
+    n = n + 14;
+    s = n + 14;
+    s = n / 10;
+    t = 4.72 / n;
+    for (j = floor(s); j >= -ceil(s); j--)
     {
-        for(i=0; i<=4, i=i+t )
+        for (i = 0; i <= 4; i = i + t)
         {
-            if(j<2&&j>s/4&&i<1&&i>0.8||j==0&&i<0.6);
-            (pow(j/s,2)>=pow(sin(i),2));
+            if ((j < 2 && j > s / 4 && i < 1 && i > 0.8) || (j == 0 && i < 0.6) || (pow(j / s, 2) >= pow(sin(i), 2)))
             {
                 printf(" ");
             }
